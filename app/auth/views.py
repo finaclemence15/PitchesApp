5a9ce30346e691b4a46438822dee903dc02cce59
finaclemence15 <finaclemence15@gmail.com>
2019-09-20 15:26:58 +0200

    sending email

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,10 +1,10 @@
 from . import auth
-from flask import render_template,redirect,url_for,flash,request
+from flask import render_template,redirect,url_for,flash
 from ..models import User
 from .forms import RegistrationForm,LoginForm
 from flask_login import login_user,logout_user,login_required
 from .. import db
-# from ..email import mail_message
+from ..email import mail_message
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
@@ -31,7 +31,7 @@ def register():
         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
-        mail_message("Welcome to watchlist","email/welcome_user",user.email,user=user)
+        mail_message("Welcome to Picthes sites","email/welcome_user",user.email,user=user)
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)
---
+++ start.sh
@@ -0,0 +1,4 @@
+export SECRET_KEY=5555
+export MAIL_USERNAME=finakagabo@gmail.com
+export MAIL_PASSWORD=Kigali100@
+python3.6 manage.py server
