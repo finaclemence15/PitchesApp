5ff4bb33e1c64794db01a743ce4167f670cfd0c1
finaclemence15 <finaclemence15@gmail.com>
2019-09-20 10:42:32 +0200

    creating model User

---
+++ config.py
@@ -0,0 +1,34 @@
+import os
+
+class Config:
+
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://clemence:34003@localhost/pitches'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+     # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+    print(MAIL_USERNAME)
+
+
+class ProdConfig(Config):
+    pass
+# class TestConfig(Config):
+#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://clemence:34003@localhost/watchlist_test'
+
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://clemence:34003@localhost/pitches'
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+# 'test':TestConfig
+}
