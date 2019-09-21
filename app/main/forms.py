d2d8619ce5adb7ac5447965cf28e5af3fb25ae2e
finaclemence15 <finaclemence15@gmail.com>
2019-09-22 00:26:20 +0200

    migrating pitches table

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,13 +1,36 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField
-from wtforms.validators import Required
+from wtforms import StringField,TextAreaField,SubmitField,PasswordField,BooleanField,RadioField
+from wtforms.validators import Required,Email,EqualTo
+from wtforms import ValidationError
 
+# class PitchForm(FlaskForm):
+
+#     title = StringField('Review title',validators=[Required()])
+#     review = TextAreaField('Movie review')
+#     submit = SubmitField('Submit')
+
+
+        # 
 class PitchForm(FlaskForm):
-
-    title = StringField('Review title',validators=[Required()])
-    review = TextAreaField('Movie review')
-    submit = SubmitField('Submit')
-    
+	title = StringField('Title', validators=[Required()])
+	description = TextAreaField("What would you like to pitch ?",validators=[Required()])
+	category = RadioField('Label', choices=[ ('promotionpitch','promotionpitch'), ('interviewpitch','interviewpitch'),('pickuplines','pickuplines'),('productpitch','productpitch')],validators=[Required()])
+	submit = SubmitField('Submit')
+        # 
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [Required()])
     submit = SubmitField('Submit')
+    
+        # 
+class CommentForm(FlaskForm):
+	description = TextAreaField('Add comment',validators=[Required()])
+	submit = SubmitField()
+
+class UpvoteForm(FlaskForm):
+	submit = SubmitField()
+
+
+class Downvote(FlaskForm):
+	submit = SubmitField()
+ 
+        #  
--- app/models.py
+++ app/models.py
@@ -14,7 +14,7 @@ class User(UserMixin,db.Model):
     pass_secure = db.Column(db.String(255))
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
-    pitch = db.relationship('Pitch',backref = 'user',lazy="dynamic")
+    pitches = db.relationship('Pitch',backref = 'user',lazy="dynamic")
     
     def __repr__(self):
       return f'User {self.username}'
@@ -42,7 +42,7 @@ class Category(db.Model):
 
     id = db.Column(db.Integer,primary_key = True)
     cat_name = db.Column(db.String(255))
-    pitch = db.relationship('Pitch',backref = 'category',lazy="dynamic") 
+    pitches = db.relationship('Pitch',backref = 'category',lazy="dynamic") 
 
 
     def __repr__(self):
@@ -50,14 +50,23 @@ class Category(db.Model):
 
 class Pitch(db.Model):
 
-    __tablename__ = 'pitch'
+    __tablename__ = 'pitches'
 
-    id = db.Column(db.Integer,primary_key = True)
-    pitch_title = db.Column(db.String)
-    your_pitch = db.Column(db.String)
-    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
-    category_id = db.Column(db.Integer,db.ForeignKey("category.id"))
-    
+    # id = db.Column(db.Integer,primary_key = True)
+    # pitch_title = db.Column(db.String)
+    # your_pitch = db.Column(db.String)
+    # user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    # category_id = db.Column(db.Integer,db.ForeignKey("category.id"))
+                    # 
+    id = db.Column(db.Integer, primary_key = True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable = False)
+    description = db.Column(db.String(), index = True)
+    title = db.Column(db.String())
+    category = db.Column(db.String(255), nullable=False)
+    comments = db.relationship('Comment',backref='pitch',lazy='dynamic')
+    upvotes = db.relationship('Upvote', backref = 'pitch', lazy = 'dynamic')
+    downvotes = db.relationship('Downvote', backref = 'pitch', lazy = 'dynamic')
+                    # 
 
        
     def save_pitch(self):
@@ -71,6 +80,84 @@ class Pitch(db.Model):
     
         
 
+        # 
+#     class Comment(db.Model):
+#     __tablename__='comments'
+    
+#     id = db.Column(db.Integer,primary_key=True)
+#     pitch_id = db.Column(db.Integer, db.ForeignKey('pitches.id'), nullable=False)
+#     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable= False)
+#     description = db.Column(db.Text)
 
+    
+#     def __repr__(self):
+#         return f"Comment : id: {self.id} comment: {self.description}"
+
+
+# class Upvote(db.Model):
+#     __tablename__ = 'upvotes'
+
+#     id = db.Column(db.Integer,primary_key=True)
+#     upvote = db.Column(db.Integer,default=1)
+#     pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
+#     user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+
+#     def save_upvotes(self):
+#         db.session.add(self)
+#         db.session.commit()
+
+
+#     def add_upvotes(cls,id):
+#         upvote_pitch = Upvote(user = current_user, pitch_id=id)
+#         upvote_pitch.save_upvotes()
+
+    
+#     @classmethod
+#     def get_upvotes(cls,id):
+#         upvote = Upvote.query.filter_by(pitch_id=id).all()
+#         return upvote
+
+#     @classmethod
+#     def get_all_upvotes(cls,pitch_id):
+#         upvotes = Upvote.query.order_by('id').all()
+#         return upvotes
+
+#     def __repr__(self):
+#         return f'{self.user_id}:{self.pitch_id}'
+
+
+
+# class Downvote(db.Model):
+#     __tablename__ = 'downvotes'
+
+#     id = db.Column(db.Integer,primary_key=True)
+#     downvote = db.Column(db.Integer,default=1)
+#     pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
+#     user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+
+#     def save_downvotes(self):
+#         db.session.add(self)
+#         db.session.commit()
+
+
+#     def add_downvotes(cls,id):
+#         downvote_pitch = Downvote(user = current_user, pitch_id=id)
+#         downvote_pitch.save_downvotes()
+
+    
+#     @classmethod
+#     def get_downvotes(cls,id):
+#         downvote = Downvote.query.filter_by(pitch_id=id).all()
+#         return downvote
+
+#     @classmethod
+#     def get_all_downvotes(cls,pitch_id):
+#         downvote = Downvote.query.order_by('id').all()
+#         return downvote
+
+#     def __repr__(self):
+#         return f'{self.user_id}:{self.pitch_id}'
+
+#                 # 
 
 
